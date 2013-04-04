b2dd3d3e0ca63bdab67b798ff941d311b1d0dc4c
Benn Eichhorn <benn@roamz.com>
2013-04-04 12:23:37 +1100

    requirements and example config

---
+++ .exampleconfig.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+
+MERCHANT_ID = "your_braintee_merchant_id"
+PUBLIC_KEY = "your_public_key"
+PRIVATE_KEY = "your_private_key"
+CLIENT_SIDE_ENCRYPTION_KEY = "your_client_side_encryption_key"
